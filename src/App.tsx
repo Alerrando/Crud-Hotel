ecf3405557f777ff382057807824c1b251c68b05
Alerrando Breno <alerrando2@gmail.com>
2022-08-10 00:52:44 -0300

    structure main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { PlusCircle } from 'phosphor-react'
+import { Pencil, PlusCircle, Trash } from 'phosphor-react'
 
 export function App() {
 
@@ -15,6 +15,43 @@ export function App() {
             </button>
         </div>
       </header>
+
+      <main className='m-8'>
+        <table className='w-full border-collapse'>
+          <thead className='bg-zinc-400 text-xl'>
+            <th className='p-1 text-start'>Id</th>
+            <th className='p-1 text-start'>Nome</th>
+            <th className='p-1 text-start'>Email</th>
+            <th className='p-1 text-start'>Telefone</th>
+            <th className='p-1 text-start'>Endereço</th>
+            <th className='p-1 text-start'>Data de Nascimento</th>
+            <th className='p-1 text-start'>Editar</th>
+            <th className='p-1 text-start'>Excluir</th>
+          </thead>
+          <tbody>
+            <tr className='border-b-2'>
+              <td className='max-h-14 p-[6px] ver'>1</td>
+              <td className='max-h-14 p-[6px] ver'>Alerrando</td>
+              <td className='max-h-14 p-[6px] ver'>alerrando2@gmail.com</td>
+              <td className='max-h-14 p-[6px] ver'>(18)99823-3887</td>
+              <td className='max-h-14 p-[6px] ver'>Rancharia</td>
+              <td className='max-h-14 p-[6px] ver'>16/10/2002</td>
+              <td className='max-h-14 p-[6px] ver'><Pencil size={32} weight="bold" /></td>
+              <td className='max-h-14 p-[6px] ver'><Trash size={32} weight="bold" /></td>
+            </tr>
+            <tr className='border-b-2'>
+              <td className='max-h-14 p-[6px] ver'>1</td>
+              <td className='max-h-14 p-[6px] ver'>Alerrando</td>
+              <td className='max-h-14 p-[6px] ver'>alerrando2@gmail.com</td>
+              <td className='max-h-14 p-[6px] ver'>(18)99823-3887</td>
+              <td className='max-h-14 p-[6px] ver'>Rancharia</td>
+              <td className='max-h-14 p-[6px] ver'>16/10/2002</td>
+              <td className='max-h-14 p-[6px] ver'><Pencil size={32} weight="bold" /></td>
+              <td className='max-h-14 p-[6px] ver'><Trash size={32} weight="bold" /></td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </>
   )
 }
