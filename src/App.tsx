454e4123def1bc6955cae1905063effb1e31c511
Alerrando Breno <alerrando2@gmail.com>
2022-08-09 21:58:43 -0300

    header site ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
+import { PlusCircle } from 'phosphor-react'
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <header className='w-full h-20 bg-[#364A54] flex items-center'>
+        <div className='w-full py-2 px-12 flex justify-between'>
+            <h1 className='text-3xl text-white'>Cadastro de Hóspedes</h1>
+
+            <button className='flex items-center gap-2 text-white bg-green-600 px-3'>
+              <PlusCircle size={26} weight="bold" />
+              Adicionar novo hóspede
+            </button>
+        </div>
+      </header>
+    </>
   )
 }
 
