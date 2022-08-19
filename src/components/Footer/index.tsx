c11ec6c22c1d49ee6ec3d7bd234e85d280224d88
Alerrando Breno <alerrando2@gmail.com>
2022-08-19 20:15:02 -0300

    responsiveness of the completed footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,21 +7,20 @@ type FooterProps = {
 export function Footer(props:FooterProps){
     return(
         <footer className="w-full h-12 bg-[#364A54] m-auto bottom-0 fixed">
-        <div className="px-12 py-3">
+        <div className="px-6 md:px-12 py-3">
           <div className="flex justify-start">
-            <div className="xl:w-96">
+            <div className="w-96">
               <div className="input-group relative flex items-center gap-2 w-full">
                 <input
                   type="search"
-                  className="form-control relative min-w-0 block py-[2px] text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  className="form-control relative min-w-0 block md:py-[2px] text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-xl transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Search"
                   onChange={(e) => props.setSearch((e.target as HTMLInputElement).value)}
                   aria-label="Search"
                   aria-describedby="button-addon2"
                 />
                 <MagnifyingGlass
-                  size={24}
-                  className="cursor-pointer text-white"
+                  className="w-4 h-4 md:w-6 md:h-6 cursor-pointer text-white"
                 />
               </div>
             </div>
